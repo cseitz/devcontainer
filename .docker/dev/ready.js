3ec557cc3a1ab324e1b5e76e80831c66b8cee02c
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-04-07 16:57:49 -0400

    adjusted ready

--- .docker/dev/ready.js
+++ .docker/dev/ready.js
@@ -3,7 +3,7 @@
 const { existsSync } = require('fs');
 
 function check() {
-    if (existsSync('/code/package.json') && existsSync('/code/pnpm-lock.yaml')) {
+    if (existsSync('/code/package.json') && existsSync('/code/pnpm-lock.yaml') && existsSync('/code/node_modules/.bin/turbo')) {
         process.exit(0);
     } else {
         setTimeout(() => {
